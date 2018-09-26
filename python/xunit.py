ac3ebdfc020ea7cd4fdbaf7d750064fdbeb13ff7
yokawara <ohkawara.dev@gmail.com>
2018-09-26 22:18:40 +0900

    implement xunit base

--- python/xunit.py
+++ python/xunit.py
@@ -1,12 +1,27 @@
-class WasRun:
+class TestCase:
+    def __init__(self, name):
+        self.name = name
+
+    def run(self):
+        method = getattr(self, self.name)
+        method()
+
+
+class WasRun(TestCase):
     def __init__(self, name):
         self.wasRun = None
+        super().__init__(name)
 
-    def TestMethod(self):
+    def testMethod(self):
         self.wasRun = 1
 
 
-test = WasRun("TestMethod")
-print(test.wasRun)
-test.TestMethod()
-print(test.wasRun)
+class TestCaseTest(TestCase):
+    def testRunning(self):
+        test = WasRun("testMethod")
+        assert(not test.wasRun)
+        test.run()
+        assert(test.wasRun)
+
+
+TestCaseTest("testRunning").run()
